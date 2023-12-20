1abc70bf097a7e4c4f3a55ef4b17694c28a36446
IR0NSIGHT <irnsght@googlemail.com>
2023-12-20 19:09:35 +0100

    add translation DIsplayOp => configOp and use it in JSON display

--- my-react-app/src/ConfigComponents/AdvancedConfigEditor.tsx
+++ my-react-app/src/ConfigComponents/AdvancedConfigEditor.tsx
@@ -22,13 +22,33 @@ function denullifyConfigArray<T>(configT: T | T[] | undefined): T[] {
     else if (Array.isArray(configT))
         return configT
     else
-        throw Error("can not translate config array!"+configT)
+        throw Error("can not translate config array!" + configT)
 }
 
 const terrainIdsToTerrains = (xs: number[]): WpTerrainType[] => {
     return xs
         .map(x => getTerrainById(x, wpTerrainTypes))
         .filter(x => x !== undefined) as WpTerrainType[]
+}
+
+export const translateDisplayOperation = (x: DisplayOperation): GlobalWpOperation => {
+    const configTerrain: number[] = [];
+    x.terrain.forEach(t => {
+        for (let i = 0; i < t.weight; i++) {
+            configTerrain.push(t.terrain.id)
+        }
+    })
+    return {
+        name: x.name,
+        aboveLevel: x.aboveLevel,
+        belowLevel: x.belowLevel,
+        aboveDegrees: x.aboveDegrees,
+        belowDegrees: x.belowDegrees,
+        perlin: x.perlin,
+        onlyOnLayer: x.layer,
+        layer: x.layer,
+        terrain: configTerrain
+    }
 }
 
 
@@ -42,7 +62,10 @@ const advancedOperationToDisplay = (configOp: GlobalWpOperation): DisplayOperati
         name: configOp.name,
         onlyOnLayer: denullifyConfigArray<WpLayerSetting>(configOp.onlyOnLayer),
         perlin: undefined,
-        terrain: terrainIdsToTerrains(denullifyConfigArray<number>(configOp.terrain)).map(t => ({weight: 1, terrain: t}))
+        terrain: terrainIdsToTerrains(denullifyConfigArray<number>(configOp.terrain)).map(t => ({
+            weight: 1,
+            terrain: t
+        }))
     }
 }
 
