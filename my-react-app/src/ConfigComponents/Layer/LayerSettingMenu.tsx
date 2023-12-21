488a756eafbfc3bfa5e4a15475a074e736a81af8
IR0NSIGHT <irnsght@googlemail.com>
2023-12-21 20:19:58 +0100

    annotations have custom value transalation strings

--- my-react-app/src/ConfigComponents/Layer/LayerSettingMenu.tsx
+++ my-react-app/src/ConfigComponents/Layer/LayerSettingMenu.tsx
@@ -1,43 +1,72 @@
 import React from "react";
 import LayerSelector from "./LayerSelector";
-import { DefaultLayers, WpLayerSetting } from "./WpLayerSetting";
-import { DefaultLayerValues, NamedValueSelector } from "./NamedValueSelector";
+import {DefaultLayers, WpLayerSetting} from "./WpLayerSetting";
+import {DefaultLayerValues, NamedNumericValue, NamedValueSelector} from "./NamedValueSelector";
 import {DeleteButton} from "../DeleteButton";
 
 export type LayerSettingMenuProps = {
-  layerSetting: WpLayerSetting;
-  onUpdateSetting: (
-    oldSetting: WpLayerSetting|null,
-    newSetting: WpLayerSetting|null
-  ) => void;
+    layerSetting: WpLayerSetting;
+    onUpdateSetting: (
+        oldSetting: WpLayerSetting | null,
+        newSetting: WpLayerSetting | null
+    ) => void;
 };
 
-const LayerSettingMenu: React.FC<LayerSettingMenuProps> = ({
-  layerSetting,
-  onUpdateSetting,
-}: LayerSettingMenuProps) => {
-  const onNameChange = (name: string) => {
-    onUpdateSetting(layerSetting, [name, layerSetting[1]]);
-  };
-  const onValueChange = (value: string) => {
-    console.log("on value change: newValue", value);
-    onUpdateSetting(layerSetting, [layerSetting[0], parseInt(value)]);
-  };
-  return (
-    <div>
-      <LayerSelector
-        layerName={layerSetting[0]}
-        layerList={DefaultLayers}
-        onUpdateLayerName={onNameChange}
-      />
-      <NamedValueSelector
-        layerValue={{ name: layerSetting[0], value: layerSetting[1] }}
-        allowedValues={DefaultLayerValues}
-        onUpdateValue={onValueChange}
-      />
-        <DeleteButton onClick={()=>{onUpdateSetting(layerSetting,null)}}/>
-    </div>
-  );
-};
+const annotationValues: NamedNumericValue[] = [
+    {value: -1, name: "Present"},
+    {value: 0, name: "Absent"},
+    {value: 1, name: "White"},
+    {value: 2, name: "Orange"},
+    {value: 3, name: "Magenta"},
+    {value: 4, name: "Light Blue"},
+    {value: 5, name: "Yellow"},
+    {value: 6, name: "Lime"},
+    {value: 7, name: "Pink"},
+    {value: 8, name: "Light Grey"},
+    {value: 9, name: "Cyan"},
+    {value: 10, name: "Purple"},
+    {value: 11, name: "Blue"},
+    {value: 12, name: "Brown"},
+    {value: 13, name: "Green"},
+    {value: 14, name: "Red"},
+    {value: 15, name: "Black"},
+]
+
+const getNamedValuesByLayer = (layerName: string): NamedNumericValue[] => {
+    switch (layerName) {
+        case "Annotations":
+            return annotationValues
+        default:
+            return DefaultLayerValues
+    }
+}
+
+const LayerSettingMenu: React.FC<LayerSettingMenuProps> =
+    ({layerSetting, onUpdateSetting,}: LayerSettingMenuProps) => {
+        const onNameChange = (name: string) => {
+            onUpdateSetting(layerSetting, [name, layerSetting[1]]);
+        };
+        const onValueChange = (value: string) => {
+            console.log("on value change: newValue", value);
+            onUpdateSetting(layerSetting, [layerSetting[0], parseInt(value)]);
+        };
+        return (
+            <div>
+                <LayerSelector
+                    layerName={layerSetting[0]}
+                    layerList={DefaultLayers}
+                    onUpdateLayerName={onNameChange}
+                />
+                <NamedValueSelector
+                    layerValue={{name: layerSetting[0], value: layerSetting[1]}}
+                    allowedValues={getNamedValuesByLayer(layerSetting[0])}
+                    onUpdateValue={onValueChange}
+                />
+                <DeleteButton onClick={() => {
+                    onUpdateSetting(layerSetting, null)
+                }}/>
+            </div>
+        );
+    };
 
 export default LayerSettingMenu;
