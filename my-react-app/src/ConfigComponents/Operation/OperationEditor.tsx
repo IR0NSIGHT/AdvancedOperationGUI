b7f9bff21a4a476ce4862a8a5abff815059264f5
IR0NSIGHT <irnsght@googlemail.com>
2023-12-21 12:42:21 +0100

    use grid in operationEditor to organize components visually

--- my-react-app/src/ConfigComponents/Operation/OperationEditor.tsx
+++ my-react-app/src/ConfigComponents/Operation/OperationEditor.tsx
@@ -1,112 +1,137 @@
 import React from "react";
-import { applyLayerChange, WpLayerSetting } from "../Layer/WpLayerSetting";
-import { CollapsibleComponent } from "./CollapsibleComponent";
-import { LayerSettingsMode, LayerListEditor } from "../Layer/LayerListEditor";
+import {applyLayerChange, WpLayerSetting} from "../Layer/WpLayerSetting";
+import {CollapsibleComponent} from "./CollapsibleComponent";
+import {LayerSettingsMode, LayerListEditor} from "../Layer/LayerListEditor";
 import {
-  OperationTerrainList,
-  TerrainSettingsMode,
-  updateTerrainList,
+    OperationTerrainList,
+    TerrainSettingsMode,
+    updateTerrainList,
 } from "../Terrain/OperationTerrainList";
-import { WeightedTerrainSetting } from "../Terrain/TerrainSettingMenu";
+import {WeightedTerrainSetting} from "../Terrain/TerrainSettingMenu";
 import {
-  DisplayOperation,
-  translateDisplayOperation,
+    DisplayOperation,
+    translateDisplayOperation,
 } from "./DisplayOperation";
+import {Grid, Paper} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 
 export type OperationEditorProps = {
-  initialOperation: DisplayOperation;
-  updateOperation: (op: DisplayOperation) => void;
-  deleteOperation: (op: DisplayOperation) => void;
+    initialOperation: DisplayOperation;
+    updateOperation: (op: DisplayOperation) => void;
+    deleteOperation: (op: DisplayOperation) => void;
 };
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+}));
+
 export const OperationEditor: React.FC<OperationEditorProps> = ({
-  initialOperation,
-  updateOperation,
-}) => {
-  const handleTerrainChanged = (
-    oldSetting: WeightedTerrainSetting | null,
-    newSetting: WeightedTerrainSetting | null
-  ) => {
-    updateOperation({
-      ...initialOperation,
-      terrain: updateTerrainList(
-        oldSetting,
-        newSetting,
-        initialOperation.terrain
-      ),
-    });
-  };
-
-  const updateApplyLayer = (
-    oldSetting: WpLayerSetting | null,
-    newSetting: WpLayerSetting | null
-  ) => {
-    const newOp = {
-      ...initialOperation,
-      layer: applyLayerChange(initialOperation.layer, oldSetting, newSetting),
+                                                                    initialOperation,
+                                                                    updateOperation,
+                                                                }) => {
+    const handleTerrainChanged = (
+        oldSetting: WeightedTerrainSetting | null,
+        newSetting: WeightedTerrainSetting | null
+    ) => {
+        updateOperation({
+            ...initialOperation,
+            terrain: updateTerrainList(
+                oldSetting,
+                newSetting,
+                initialOperation.terrain
+            ),
+        });
     };
-    updateOperation(newOp);
-  };
 
-  const updateOnlyOnLayer = (
-    oldSetting: WpLayerSetting | null,
-    newSetting: WpLayerSetting | null
-  ) => {
-    const newOp = {
-      ...initialOperation,
-      onlyOnLayer: applyLayerChange(
-        initialOperation.onlyOnLayer,
-        oldSetting,
-        newSetting
-      ),
+    const updateApplyLayer = (
+        oldSetting: WpLayerSetting | null,
+        newSetting: WpLayerSetting | null
+    ) => {
+        const newOp = {
+            ...initialOperation,
+            layer: applyLayerChange(initialOperation.layer, oldSetting, newSetting),
+        };
+        updateOperation(newOp);
     };
-    updateOperation(newOp);
-  };
 
-  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    updateOperation({ ...initialOperation, name: event.target.value });
-  };
+    const updateOnlyOnLayer = (
+        oldSetting: WpLayerSetting | null,
+        newSetting: WpLayerSetting | null
+    ) => {
+        const newOp = {
+            ...initialOperation,
+            onlyOnLayer: applyLayerChange(
+                initialOperation.onlyOnLayer,
+                oldSetting,
+                newSetting
+            ),
+        };
+        updateOperation(newOp);
+    };
 
-  return (
-    <CollapsibleComponent
-      title={initialOperation.name}
-      content={
-        <div>
-          <div>
-            <label htmlFor="nameInput">Operation Name:</label>
-            <input
-              type="text"
-              id="nameInput"
-              value={initialOperation.name}
-              onChange={handleNameChange}
-            />
-          </div>
-          <LayerListEditor
-            mode={LayerSettingsMode.APPLY}
-            layers={initialOperation.layer}
-            updateLayer={updateApplyLayer}
-          />
+    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        updateOperation({...initialOperation, name: event.target.value});
+    };
+    const classes = useStyles();
+    return (
+        <CollapsibleComponent
+            title={initialOperation.name}
+            content={
+                <div>
+                    <div>
+                        <label htmlFor="nameInput">Operation Name:</label>
+                        <input
+                            type="text"
+                            id="nameInput"
+                            value={initialOperation.name}
+                            onChange={handleNameChange}
+                        />
+                    </div>
+                    <div className={classes.root}>
+                        <Grid container spacing={2}>
 
-          <OperationTerrainList
-            mode={TerrainSettingsMode.APPLY}
-            terrains={initialOperation.terrain}
-            onTerrainChanged={handleTerrainChanged}
-          />
+                            <Grid item xs={6}>
+                                <LayerListEditor
+                                    mode={LayerSettingsMode.APPLY}
+                                    layers={initialOperation.layer}
+                                    updateLayer={updateApplyLayer}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <LayerListEditor
+                                    mode={LayerSettingsMode.ONLY_ON_LAYER}
+                                    layers={initialOperation.onlyOnLayer}
+                                    updateLayer={updateOnlyOnLayer}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <OperationTerrainList
+                                    mode={TerrainSettingsMode.APPLY}
+                                    terrains={initialOperation.terrain}
+                                    onTerrainChanged={handleTerrainChanged}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <OperationTerrainList
+                                    mode={TerrainSettingsMode.ONLY_ON_TERRAIN}
+                                    terrains={initialOperation.terrain}
+                                    onTerrainChanged={handleTerrainChanged}
+                                />
+                            </Grid>
+                        </Grid>
+                    </div>
 
-          <LayerListEditor
-            mode={LayerSettingsMode.ONLY_ON_LAYER}
-            layers={initialOperation.onlyOnLayer}
-            updateLayer={updateOnlyOnLayer}
-          />
-          <pre>
-            {JSON.stringify(
-              translateDisplayOperation(initialOperation),
-              null,
-              3
-            )}
-          </pre>
-        </div>
-      }
-    />
-  );
+                    <CollapsibleComponent title={"JSON config"} content={<pre>
+                        {JSON.stringify(
+                            translateDisplayOperation(initialOperation),
+                            null,
+                            3
+                        )}
+                    </pre>} ></CollapsibleComponent>
+                </div>
+            }
+        />
+    );
 };
