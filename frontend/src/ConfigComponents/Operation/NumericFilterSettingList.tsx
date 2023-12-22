a3894011ed5fb6da9699cece470f706d519473d7
IR0NSIGHT <irnsght@googlemail.com>
2023-12-22 15:14:02 +0100

    working numeric filters type selction (no values yet)

--- frontend/src/ConfigComponents/Operation/NumericFilterSettingList.tsx
+++ frontend/src/ConfigComponents/Operation/NumericFilterSettingList.tsx
@@ -16,15 +16,25 @@ type NumericFilterSettingListProps = {
 export const NumericFilterSettingList: React.FC<
   NumericFilterSettingListProps
 > = ({ allowedFilters, listedFilters, onFiltersChanged }) => {
-  const setFilterType = (idx: number, filter: NumericFilter) => {
-    const newFilters = [...listedFilters];
-    newFilters[idx] = { filter: filter, value: newFilters[idx].value };
-    onFiltersChanged(listedFilters, newFilters);
+  const setFilterType = (
+    oldF: NumericFilter | undefined,
+    newF: NumericFilter | undefined
+  ) => {
+    const others = listedFilters.filter(
+      (f) => f.filter.name != oldF?.name && f.filter.name != newF?.name
+    );
+    const out =
+      newF !== undefined
+        ? [...others, { filter: newF, value: 0 }].sort((a, b) =>
+            a.filter.name.localeCompare(b.filter.name)
+          )
+        : others;
+    onFiltersChanged(listedFilters, out);
   };
 
   const addFilter = () => {
     const newFilters = [
-      ...listedFilters,
+      ...listedFilters.filter((t) => t.filter.name != allowedFilters[0].name),
       {
         filter: allowedFilters[0],
         value: 0,
@@ -33,8 +43,11 @@ export const NumericFilterSettingList: React.FC<
     onFiltersChanged(listedFilters, newFilters);
   };
 
-  const deleteFilter = (id: number) => {
-    onFiltersChanged(listedFilters, listedFilters.splice(id, 1));
+  const deleteFilter = (f: NumericFilter) => {
+    onFiltersChanged(
+      listedFilters,
+      listedFilters.filter((x) => x.filter.name !== f.name)
+    );
   };
 
   const filters = listedFilters.map((f, idx) => (
@@ -42,9 +55,9 @@ export const NumericFilterSettingList: React.FC<
       <NumericFilterSelect
         selectedFilter={f.filter}
         allowedFilters={allowedFilters}
-        onFilterChanged={(old, newF) => setFilterType(idx, newF)}
+        onFilterChanged={setFilterType}
       />
-      <DeleteButton onClick={() => deleteFilter(idx)} />
+      <DeleteButton onClick={() => deleteFilter(f.filter)} />
     </div>
   ));
   return (
