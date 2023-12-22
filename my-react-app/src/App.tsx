e353f6aa1a6129d8514e6fe1f4e02386f5ca86e6
IR0NSIGHT <irnsght@googlemail.com>
2023-12-22 10:18:39 +0100

    add icyPines as default config to app

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,33 +1,10 @@
 import { AdvancedConfigEditor } from "./ConfigComponents/AdvancedConfigEditor";
-import { formatDate } from "@storybook/blocks";
-import {DisplayConfig} from "./ConfigComponents/DisplayConfig";
-import {
-  ConfigOperation,
-  configOperationToDisplay,
-  emptyConfigOperation
-} from "./ConfigComponents/Operation/ConfigOperation";
+import {IcyPinesConfig, rawConfigToDisplay} from "./ConfigComponents/AdvancedConfigEditor.stories";
 
-const ops : ConfigOperation[] = [{
-  ...emptyConfigOperation,
-  name: "My first operation",
-  terrain: [0, 0, 0, 1, 2, 3],
-  layer: [
-    ["Pines", 5],
-    ["Deciduous", 7],
-    ["Annotations", 1],
-  ],
-}]
-const initialConfig: DisplayConfig = {
-  operations: ops.map(configOperationToDisplay),
-  name: "DebugConfig",
-  author: "IR0NSIGHT",
-  date: formatDate(Date.now()),
-  description: "owo!"
-};
 function App() {
   return (
     <div className="App">
-      <AdvancedConfigEditor initialConfig={initialConfig} />
+      <AdvancedConfigEditor initialConfig={rawConfigToDisplay(IcyPinesConfig)} />
     </div>
   );
 }
